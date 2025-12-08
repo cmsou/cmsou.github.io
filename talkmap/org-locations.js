a86aac2d39d529541a35f3a8d8c177f5e6282887
cmsou <73458243+cmsou@users.noreply.github.com>
2025-12-08 20:20:54 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,10 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "Seminar talk at Tsinghua University",
+   	40.0022905,116.32096299999999
+  ],
+  [
+    "Workshop talk at Yukawa Institute for Theoretical Physics, Kyoto University",
+   	35.0270467,135.7814382
+  ]
+];
